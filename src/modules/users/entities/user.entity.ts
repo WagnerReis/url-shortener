5d17f658d1afcdfbea21d68301e9f18cc954be49
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-16 19:04:00 -0300

    feat(users): add User entity and repository interface

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity } from '@/core/entities/entity';
+
+interface UserProps {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class User extends Entity<UserProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
