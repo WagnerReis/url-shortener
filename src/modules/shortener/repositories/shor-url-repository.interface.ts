260a6fc4c7ac744d105ea1b0a7c781fead508ffb
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-17 17:56:11 -0300

    feat(shortener): add ShortUrlRepositoryInterface

---
+++ src/modules/shortener/repositories/shor-url-repository.interface.ts
@@ -0,0 +1,7 @@
+import { ShortUrl } from '../entities/short-url.entity';
+
+export abstract class ShortUrlRepositoryInterface {
+  abstract create(shortUrl: ShortUrl): Promise<void>;
+  abstract findByShortCode(shortCode: string): Promise<ShortUrl | null>;
+  abstract save(shortUrl: ShortUrl): Promise<void>;
+}
