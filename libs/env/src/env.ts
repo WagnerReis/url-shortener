0836465936e419ecbbee2bf038fa47d9516ec3b6
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-16 21:55:08 -0300

    feat(env): add DATABASE_URL to environment schema

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
+  DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
   JWT_EXPIRATION: z.coerce
     .number()
