4e4ad5abb1a6fa604a82793cb2d27c5e3c28850c
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-16 22:56:45 -0300

    feat(error-handling): implement HttpExceptionFilter and enhance server bootstrap

---
+++ src/core/filters/http-exception.filter.ts
@@ -0,0 +1,49 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const res = exception.getResponse();
+      message =
+        typeof res === 'string'
+          ? res
+          : (res as any).message || exception.message;
+    } else if (exception instanceof Error) {
+      message = exception.message;
+    }
+
+    this.logger.error(`HTTP ${status} Error: ${message}`, {
+      path: request.url,
+      method: request.method,
+      ip: request.ip,
+      userAgent: request.get('User-Agent'),
+      stack: exception instanceof Error ? exception.stack : undefined,
+    });
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { EnvService } from '@env/env';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const envService = app.get(EnvService);
+  const port = envService.get('PORT');
+  const logger = new Logger('NestApplication');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(port);
+
+  logger.log(`Server is running on http://localhost:${port}`);
 }
-bootstrap();
+void bootstrap();
