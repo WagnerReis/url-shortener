ddaabb3b3475ba3ba349c7b2ef353267fb63d591
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-17 17:56:48 -0300

    feat(test): add in-memory short URL repository for testing

---
+++ test/repositories/in-memory-short-url.repository.ts
@@ -0,0 +1,29 @@
+import { ShortUrl } from '@/modules/shortener/entities/short-url.entity';
+import { ShortUrlRepositoryInterface } from '@/modules/shortener/repositories/shor-url-repository.interface';
+
+export class InMemoryShortUrlRepository implements ShortUrlRepositoryInterface {
+  public shortUrls: ShortUrl[] = [];
+
+  async create(shortUrl: ShortUrl): Promise<void> {
+    await Promise.resolve(this.shortUrls.push(shortUrl));
+  }
+
+  async findByShortCode(shortCode: string): Promise<ShortUrl | null> {
+    const shortUrl = this.shortUrls.find(
+      (item) => item.shortCode === shortCode,
+    );
+
+    if (!shortUrl) {
+      return Promise.resolve(null);
+    }
+
+    return Promise.resolve(shortUrl);
+  }
+
+  async save(shortUrl: ShortUrl): Promise<void> {
+    const index = this.shortUrls.findIndex((item) => item.id === shortUrl.id);
+
+    this.shortUrls[index] = shortUrl;
+    await Promise.resolve();
+  }
+}
