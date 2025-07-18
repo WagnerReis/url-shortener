0b889e170979fed8d57cb0c894b665abd9a66819
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-18 10:31:11 -0300

    test(shortener): add unit tests for RedirectShortUrlUseCase

---
+++ src/modules/shortener/usecases/redirect-short-url.usecase.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryShortUrlRepository } from 'test/repositories/in-memory-short-url.repository';
+import { ShortUrl } from '../entities/short-url.entity';
+import { NotFoundError } from './errors/not-found.error';
+import { RedirectShortUrlUseCase } from './redirect-short-url.usecase';
+
+describe('RedirectShortUrlUseCase', () => {
+  let shortUrlRepository: InMemoryShortUrlRepository;
+  let sut: RedirectShortUrlUseCase;
+
+  beforeEach(() => {
+    shortUrlRepository = new InMemoryShortUrlRepository();
+    sut = new RedirectShortUrlUseCase(shortUrlRepository);
+  });
+
+  it('deve incrementar o clickCount e retornar a URL original', async () => {
+    const shortUrl = ShortUrl.create({
+      originalUrl: 'https://www.google.com',
+      shortCode: 'abc123',
+    });
+    await shortUrlRepository.create(shortUrl);
+    const result = await sut.execute({ shortCode: 'abc123' });
+    expect(result.isRight()).toBeTruthy();
+    if (result.isRight()) {
+      expect(result.value.originalUrl).toBe('https://www.google.com');
+    }
+    expect(shortUrlRepository.shortUrls[0].clickCount).toBe(1);
+  });
+
+  it('deve retornar NotFoundError se o shortCode não existir', async () => {
+    const result = await sut.execute({ shortCode: 'not-exist' });
+    expect(result.isLeft()).toBeTruthy();
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(NotFoundError);
+    }
+  });
+});
