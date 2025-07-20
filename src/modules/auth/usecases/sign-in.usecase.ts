306b0024f07643ac6012d883b0f94e59a3f8aaa5
Wagner Reis <wagnerreis1997@gmail.com>
2025-07-19 21:43:15 -0300

    feat(auth): add error handling and HashingError to sign-in usecase

--- src/modules/auth/usecases/sign-in.usecase.ts
+++ src/modules/auth/usecases/sign-in.usecase.ts
@@ -4,6 +4,7 @@ import { Either, left, right } from '@/core/either';
 import { UserRepositoryInterface } from '@/modules/users/repositories/user-repository.interface';
 import { Injectable } from '@nestjs/common';
 import { JwtSignOptions } from '@nestjs/jwt';
+import { HashingError } from './errors/hashing.error';
 import { UnauthorizedError } from './errors/unauthorized.error';
 
 type SignInResponse = Either<
@@ -22,26 +23,33 @@ export class SignInUseCase {
   ) {}
 
   async execute(email: string, password: string): Promise<SignInResponse> {
-    const user = await this.userRepository.findByEmail(email);
+    try {
+      const user = await this.userRepository.findByEmail(email);
 
-    if (!user) {
-      return left(new UnauthorizedError('User not found.'));
+      if (!user) {
+        return left(new UnauthorizedError('User not found.'));
+      }
+
+      const isPasswordValid = this.hashComparer.compare(
+        password,
+        user.password,
+      );
+
+      if (!isPasswordValid) {
+        return left(new UnauthorizedError('Invalid password.'));
+      }
+
+      const payload = { sub: user.id.toString(), email: user.email };
+
+      const accessToken = await this.encrypter.encrypt(payload, {
+        expiresIn: '1d',
+      });
+
+      return right({
+        accessToken,
+      });
+    } catch (error) {
+      return left(new HashingError(`Error signing in: ${error}`));
     }
-
-    const isPasswordValid = this.hashComparer.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return left(new UnauthorizedError('Invalid password.'));
-    }
-
-    const payload = { sub: user.id.toString(), email: user.email };
-
-    const accessToken = await this.encrypter.encrypt(payload, {
-      expiresIn: '1d',
-    });
-
-    return right({
-      accessToken,
-    });
   }
 }
